ccfdf732a413dda5dca9d0008929f66fc6d82c75
moharick <moharick@gmail.com>
2019-10-16 10:20:57 +0300

    This commit adds the first view.

--- clone/views.py
+++ clone/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.conf import settings
+from django.http import HttpResponse
+
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+    image_form = PostForm()
+    images = Post.objects.all()
+    commentform = CommentForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            request.user.profile.post(form)
+    return render(request, 'home.html')
+
