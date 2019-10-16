3ce695833d853b484a8a9a7687c3cdf834e32496
moharick <moharick@gmail.com>
2019-10-16 12:07:53 +0300

    This commit adds the first url pattern.

--- clone/urls.py
+++ clone/urls.py
@@ -2,3 +2,7 @@ from django.conf.urls import urls
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+urlpatterns = [
+     url(r'^$', views.home, name='home'),
+]
