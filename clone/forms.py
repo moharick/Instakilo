3ab39cbe57fabf9230d7279b20e226db965bdde1
moharick <moharick@gmail.com>
2019-10-17 08:31:49 +0300

    This commit adds a signup form.

--- clone/forms.py
+++ clone/forms.py
@@ -1,18 +1,25 @@
 from django import forms
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('image', 'caption')
 
-class ProfileForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['Name'].widget=forms.TextInput()
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
     class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+class ProfileForm(forms.ModelForm):
+   class Meta:
         model = Profile
-        fields = ('Name','profile_picture','bio' )
+        fields = ['name', 'profile_picture', 'bio']
 
 
 class CommentForm(forms.ModelForm):
